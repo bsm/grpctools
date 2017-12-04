52a6657cf1b8131713809039cd2745bb1b332a64
Max Cherry <mxmCherry@gmail.com>
2017-12-04 12:10:15 +0200

    Bump bsm/rucksack

--- instrument.go
+++ instrument.go
@@ -7,9 +7,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/bsm/rucksack/log"
 	"github.com/bsm/rucksack/met"
+	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -54,7 +54,7 @@ func (i *Instrumenter) StreamServerInterceptor(srv interface{}, stream grpc.Serv
 func (i *Instrumenter) instrument(name string, err error, elapsed time.Duration) {
 	errtags := extractErrorTags(err)
 	status := HTTPStatusFromError(err)
-	logger := log.WithField("status", status)
+	logger := log.L().With(zap.Int("status", status)).Sugar()
 	mtags := []string{
 		"rpc:" + name,
 		"status:" + strconv.Itoa(status),
@@ -72,7 +72,7 @@ func (i *Instrumenter) instrument(name string, err error, elapsed time.Duration)
 
 func (i *Instrumenter) recover(name string) {
 	if r := recover(); r != nil {
-		log.WithField("rpc", name).Errorf("panic: %v\n%v", r, debug.Stack())
+		log.L().With(zap.String("rpc", name)).Sugar().Errorf("panic: %v\n%v", r, debug.Stack())
 		met.RatePerMin(i.metric+".error", []string{
 			"rpc:" + name,
 			"status:500",
@@ -93,11 +93,11 @@ func extractErrorTags(err error) []string {
 	return tags
 }
 
-func loggerWithTags(ent *logrus.Entry, tags []string) *logrus.Entry {
+func loggerWithTags(l *zap.SugaredLogger, tags []string) *zap.SugaredLogger {
 	for _, t := range tags {
 		if parts := strings.SplitN(t, ":", 2); len(parts) == 2 {
-			ent = ent.WithField(parts[0], parts[1])
+			l = l.With(zap.String(parts[0], parts[1]))
 		}
 	}
-	return ent
+	return l
 }
